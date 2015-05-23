024997d63e22d5d308ed31422a1daba2d6af7db3
José Luis Cruz <joseluis.cruz@murciaeduca.es>
2015-05-23 09:11:42 +0200

    Agregados comentarios sobre lo realizado
    Comentarios internos, pero también para Javadoc. De paso, se ha
    eliminado el evento "LostFocus" que se había puesto de prueba, en clase.

--- EncuentraBombas/src/elementos/Casilla.java
+++ EncuentraBombas/src/elementos/Casilla.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package elementos;
 
 import java.awt.GridLayout;
@@ -11,30 +6,45 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 /**
- *
- * @author jlcruz-cifp
+ * Gestiona cada casilla del tablero de juego.
+ * 
+ * Las casillas se crean desde la clase "Tablero" de este mismo paquete.
+ * 
+ * @author La clase de 1º DAW y José Luis Cruz
  */
 public class Casilla
 {
-    private JPanel hueco;
-    private JLabel rotulo;
-    private JButton boton;
+    // Elementos visuales que componen la casilla:
+    private JPanel hueco;  // Panel base que sirve de contenedor al resto
+    private JLabel rotulo; // Puede ser un número o bien la letra "B" (bomba aparecida) o "X" marca de bomba puesta por el jugador
+    private JButton boton; // Botón superficial que aparece al principio y cuya pulsación destapa el posible rótulo
     
+    /**
+     * Crea una nueva casilla, que deberá ser agregada a un tablero mediante getHueco()
+     */
     public Casilla()
     {
+        // creamos los componentes visuales
         hueco = new JPanel();
         rotulo = new JLabel ();
         boton = new JButton ();
         
+        // establecemos el diseño del hueco como una rejilla de una celda
         hueco.setLayout(new GridLayout(1, 1));
+        // le agregamos el rótulo, pero lo ponemos invisible (de momento)
         hueco.add(rotulo);
         rotulo.setVisible(false);
-        
+
+        // también agregamos el botón al hueco base, pero esta vez visible
         hueco.add(boton);
         boton.setVisible(true);
         
     }
     
+    /**
+     * Sirve para alcanzar la referencia al panel "hueco" contenedor de la casilla
+     * @return Referencia al objeto JPanel contenedor de la casilla
+     */
     public JPanel getHueco()
     {
         return this.hueco;
--- EncuentraBombas/src/elementos/Tablero.java
+++ EncuentraBombas/src/elementos/Tablero.java
@@ -1,60 +1,67 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package elementos;
 
 import java.awt.GridLayout;
 import javax.swing.JPanel;
 
 /**
- *
- * @author jlcruz-cifp
+ * Implemnta la funcionalidad de un tablero de juego y del juego en sí mismo.
+ *  
+ * @author La clase de 1º DAW y José Luis Cruz
  */
 public class Tablero
 {
-    private int ancho;
-    private int alto;
-    private int numero_minas;
-    private int minas_marcadas;
+    // propiedades privadas, que reflejan características y el estado de juego
+    private int ancho; // anchura (en casillas) del tablero
+    private int alto;  // altura (en casillas) del tablero
+    private int numero_bombas;   // cantidad de bombas que hay en el tablero
+    private int bombas_marcadas; // cantidad de bombas marcadas por el jugador
+    
     private int estado_juego;
     /*
-    * Estado del juego, que puede ser:
+    * Identifica el estado en el que se encuentra el juego, que puede ser:
     *  0 - Inicial, el contador aún no corre
     *  1 - Jugando, el contador está corriendo
     *  2 - Pausado, el contador se detiene
     *  3 - Finalizado, juego completado correctamente
     *  4 - Terminado prematuramente por encontrar bomba
     */
-    private Casilla[][] casillas;
+    
+    private Casilla[][] casillas; // array bidimensional con las casillas del tablero
+    private JPanel lienzo; // mantiene una referencia al panel principal en el que se pintan las casillas
+
     /**
+     * Construye un objeto tablero de juego, con los datos que se le proporcionan.
      * 
-     * @param ancho unidades de anchura (en celdas)
-     * @param alto  unidades de altura (en celdas)
-     */
-    
-    private JPanel lienzo;
-    
+     * @param ancho  unidades de anchura para el tablero (en celdas)
+     * @param alto   unidades de altura para el tablero (en celdas)
+     * @param lienzo panel contenedor principal de todas las casillas
+     */  
     public Tablero(int ancho, int alto, JPanel lienzo)
     {
+        // recordar los parámetros del constructor en propiedades privadas
         this.ancho = ancho;
         this.alto = alto;
         this.lienzo = lienzo;
-        this.numero_minas = (int)Math.round((ancho * alto) * 0.05);
-        this.minas_marcadas = 0;
+        // calcular la cantidad de bombas como un 5% del total de casillas
+        this.numero_bombas = (int)Math.round((ancho * alto) * 0.05);
+        // valores iniciales para el resto de propiedades privadas
+        this.bombas_marcadas = 0;
         this.estado_juego = 0;
+        // crear dinámicamente la tabla de casillas:
         casillas = new Casilla[ancho][alto];
+        // establecer el diseño del panel de casillas como una rejilla de "ancho" x "alto"
         lienzo.setLayout(new GridLayout(ancho, alto));
+        // recorrer el array para crear las casillas de su interior
         for (int x=0;x<ancho;x++)
         {
             for (int y=0;y<alto;y++)
             {
+                // crear la casilla
                 casillas[x][y] = new Casilla();
+                // agregarla al panel "lienzo" y ponerla visible
                 lienzo.add(casillas[x][y].getHueco());
                 casillas[x][y].getHueco().setVisible(true);
             }
         }
-        
     }
 }
